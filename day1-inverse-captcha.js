3f6f3fb9cbc2e28f57ea83fbd85ee3b5c0417fe8
Nick Meldrum <nick@nickmeldrum.com>
2017-12-05 15:55:17 +0000

    Refactor similarities in sum reducer funcs and make more readable

--- day1-inverse-captcha.js
+++ day1-inverse-captcha.js
@@ -1,9 +1,12 @@
-export const sumMatchingNextDigit = input => input.split('').reduce((sum, current, index, original) => {
-  const next = index + 1 === original.length ? original[0] : original[index + 1]
-  return (current === next) ? sum = sum + parseInt(current, 10) : sum
-}, 0)
+const sum = input => ({
+  matching: matcher => input.split('').reduce((sum, current, index, original) =>
+    (current === matcher(original, index)) ? sum = sum + parseInt(current, 10) : sum, 0)
+})
 
-export const sumMatchingDigitHalfWayAround = input => input.split('').reduce((sum, current, index, original) => {
-  const halfwayAround = original[(original.length / 2 + index) % original.length]
-  return (current === halfwayAround) ? sum = sum + parseInt(current, 10) : sum
-}, 0)
+export const sumMatchingNextDigit = input => sum(input).matching((original, index) =>
+  index + 1 === original.length ? original[0] : original[index + 1]
+)
+
+export const sumMatchingDigitHalfWayAround = input => sum(input).matching((original, index) =>
+  original[(original.length / 2 + index) % original.length]
+)
