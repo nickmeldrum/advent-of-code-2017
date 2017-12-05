dd0d4ae059d363c9b58c5c3263f1bdda98fe4950
Nick Meldrum <nick@nickmeldrum.com>
2017-12-05 15:28:27 +0000

    Refactor to reduce

--- day1-inverse-captcha.js
+++ day1-inverse-captcha.js
@@ -1,9 +1,4 @@
-export const sumMatchingDigits = input => {
-  let sum = 0
-  for(let position = 0; position < input.length; position++) {
-    const current = input[position]
-    const next = position + 1 === input.length ? input[0] : input[position + 1]
-    if (current === next) sum = sum + parseInt(current, 10)
-  }
-  return sum
-}
+export const sumMatchingDigits = input => input.split('').reduce((sum, current, index, original) => {
+  const next = index + 1 === original.length ? original[0] : original[index + 1]
+  return (current === next) ? sum = sum + parseInt(current, 10) : sum
+}, 0)
