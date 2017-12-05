ee3e4f9dc908233ddc91d41135885abc219969eb
Nick Meldrum <nick@nickmeldrum.com>
2017-12-05 15:41:41 +0000

    refactor data driven tests to be reusable and more readable

--- day1-inverse-captcha.js
+++ day1-inverse-captcha.js
@@ -1,4 +1,6 @@
-export const sumMatchingDigits = input => input.split('').reduce((sum, current, index, original) => {
+export const sumMatchingNextDigit = input => input.split('').reduce((sum, current, index, original) => {
   const next = index + 1 === original.length ? original[0] : original[index + 1]
   return (current === next) ? sum = sum + parseInt(current, 10) : sum
 }, 0)
+
+export const sumMatchingDigitHalfWayAround = input => input
