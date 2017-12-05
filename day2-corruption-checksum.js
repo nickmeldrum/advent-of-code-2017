7d0991ec160c0ecc99cd8e79b1c3f68d1f3a68ef
Nick Meldrum <nick@nickmeldrum.com>
2017-12-05 22:38:47 +0000

    Attempt at sum evenly divisible wip

--- day2-corruption-checksum.js
+++ day2-corruption-checksum.js
@@ -14,7 +14,18 @@ const sumDifferences = spreadsheet => spreadsheet.reduce((sum, row) => {
   return sum + max - min
 }, 0)
 
-const sumEvenly = () => 0
+const sumEvenly = spreadsheet => spreadsheet.reduce((sum, row) => {
+  for (let i = 0; i < row.length; i++) {
+    for (let j = i; j < row.length; j++) {
+      const dividend = row[i] > row[j] ? row[i] : row[j]
+      const divisor = row[i] > row[j] ? row[j] : row[i]
+      if (dividend % divisor === 0) {
+        return sum + dividend / divisor
+      }
+    }
+  }
+  throw new Error('no evenly divisble numbers found')
+}, 0)
 
 export const sumAllDifferences = (filename, cb) => {
   fs.readFile(`./${filename}.txt`, 'utf8', (err, data) => {
