c85447db5f12e7d7731346a1c7fd302cb96deb95
Nick Meldrum <nick@nickmeldrum.com>
2017-12-05 17:14:53 +0000

    Add day2 part2 failing test

--- day2-corruption-checksum.js
+++ day2-corruption-checksum.js
@@ -4,7 +4,7 @@ const parse = data => data.split('\n')
   .filter(row => row)
   .map(row => row.split('\t').map(element => parseInt(element, 10)))
 
-const sum = spreadsheet => spreadsheet.reduce((sum, row) => {
+const sumDifferences = spreadsheet => spreadsheet.reduce((sum, row) => {
   let min = Number.MAX_SAFE_INTEGER
   let max = 0
   row.forEach(element => {
@@ -14,9 +14,18 @@ const sum = spreadsheet => spreadsheet.reduce((sum, row) => {
   return sum + max - min
 }, 0)
 
+const sumEvenly = () => 0
+
 export const sumAllDifferences = (filename, cb) => {
   fs.readFile(`./${filename}.txt`, 'utf8', (err, data) => {
     if (err) throw err
-    cb(sum(parse(data)))
+    cb(sumDifferences(parse(data)))
+  })
+}
+
+export const sumEvenlyDivisibleValues = (filename, cb) => {
+  fs.readFile(`./${filename}.txt`, 'utf8', (err, data) => {
+    if (err) throw err
+    cb(sumEvenly(parse(data)))
   })
 }
--- day2-corruption-checksum.test.js
+++ day2-corruption-checksum.test.js
@@ -1,17 +1,28 @@
-import { sumAllDifferences } from 'day2-corruption-checksum'
+import { sumAllDifferences, sumEvenlyDivisibleValues } from 'day2-corruption-checksum'
 
 describe('day2 = corruption checksum', () => {
-  it('part 1 - sum all differences', done => {
-    sumAllDifferences('day2-test-input', answer => {
-      expect(answer).toEqual(18)
-      done()
+  describe('part 1 - sum all differences', () => {
+    it('test data sum returns 18', done => {
+      sumAllDifferences('day2-part1-test-input', answer => {
+        expect(answer).toEqual(18)
+        done()
+      })
+    })
+
+    it('just output the puzzle answer and pass :)', done => {
+      sumAllDifferences('day2-input', answer => {
+        console.log('And the day 2 part 1 answer is:', answer)
+        done()
+      })
     })
   })
 
-  it('just output the puzzle answer and pass :)', done => {
-    sumAllDifferences('day2-input', answer => {
-      console.log('And the day 2 part 1 answer is:', answer)
-      done()
+  describe('part 2 - sum evenly divisible values', () => {
+    it('test data sum returns 9', done => {
+      sumEvenlyDivisibleValues('day2-part2-test-input', answer => {
+        expect(answer).toEqual(18)
+        done()
+      })
     })
   })
 })
