445ffb99bb24aa7f40708e582b8e899c82f40525
Nick Meldrum <nick@nickmeldrum.com>
2017-12-06 10:22:41 +0000

    day 3 part 1 tests passing

--- day3-spiral-memory.js
+++ day3-spiral-memory.js
@@ -1,5 +1,28 @@
+const direction = ['right', 'up', 'left', 'down']
+
 export const manhattanDistance = location => {
+  let current = 0
+  let jumpInterval = 1
+  let switcher = true
+  let cornerIndex = 2
+  const changeDirectionIndices = []
+  const position = { x: 1, y: 0 }
+
   if (location === 1) return 0
 
-  return location
+  for (let i = 2; i < location; i++) {
+    switcher = !switcher
+    if (switcher) jumpInterval++
+    changeDirectionIndices.push(cornerIndex)
+    cornerIndex = cornerIndex + jumpInterval
+    if (changeDirectionIndices.includes(i)) {
+      if (current === 3) current = 0
+      else current = current + 1
+    }
+    if (direction[current] === 'right') position.x = position.x + 1
+    if (direction[current] === 'up') position.y = position.y + 1
+    if (direction[current] === 'left') position.x = position.x - 1
+    if (direction[current] === 'down') position.y = position.y - 1
+  }
+  return Math.abs(position.x) + Math.abs(position.y)
 }
--- day3-spiral-memory.test.js
+++ day3-spiral-memory.test.js
@@ -4,8 +4,12 @@ import { test } from 'test-creator'
 describe('day3 - spiral memory', () => {
   describe('part 1 - manhattan distance', () => {
     test(manhattanDistance).withData([
-      {input:1, expected: 0},
-      {input:12, expected: 3},
+      {input: 1, expected: 0},
+      {input: 2, expected: 1},
+      {input: 3, expected: 2},
+      {input: 12, expected: 3},
+      {input: 23, expected: 2},
+      {input: 1024, expected: 31},
     ])
   })
 })
