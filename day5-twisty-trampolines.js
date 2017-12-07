7dc06a078e79a1fbe83736a9a1336f1339605dee
Nick Meldrum <nick@nickmeldrum.com>
2017-12-07 09:27:15 +0000

    day5 part2 done

--- day5-twisty-trampolines.js
+++ day5-twisty-trampolines.js
@@ -1,11 +1,17 @@
-export const numberOfStepsToEscape = offsets => {
+const numberOfStepsToEscapeBasedOnOffsetModifier = offsetModifierFunc => offsets => {
   let position = 0
   let numberOfSteps = 0
   while (position >= 0 && position < offsets.length) {
     let currentOffset = offsets[position]
-    offsets[position] += 1
+    offsets[position] = offsetModifierFunc(offsets[position])
     position += currentOffset
     ++numberOfSteps
   }
   return numberOfSteps
 }
+
+const increaseBy1 = offset => offset + 1
+const over3ThenDecreaseBy1OtherwiseIncreaseBy1 = offset => offset >= 3 ? offset - 1 : offset + 1
+
+export const numberOfStepsToEscape = numberOfStepsToEscapeBasedOnOffsetModifier(increaseBy1)
+export const numberOfStepsToEscapeStrangerOffsets = numberOfStepsToEscapeBasedOnOffsetModifier(over3ThenDecreaseBy1OtherwiseIncreaseBy1)
